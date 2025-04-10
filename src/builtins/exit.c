71cb327a8558bf9945a8e03d7f45d278226cfb9a
Mohamed Aymane Zary <mzary@e2r2p4.1337.ma>
2025-04-10 18:16:06 +0100

    default message

--- src/builtins/exit.c
+++
@@ -1,7 +0,0 @@
-#include "../../inc/builtins.h"
-
-t_status	minishell_exit(t_minishell *minishell)
-{
-	minishell_cleanup(minishell, STATUS_SUCCESS);
-	return (STATUS_SUCCESS);
-}
--- src/tools/error.c
+++
@@ -1,8 +0,0 @@
-#include "../../inc/tools.h"
-
-void	minishell_error(t_status status)
-{
-    if (status != STATUS_EMPTYCMD &&
-		status != STATUS_HDOCSIGINT)
-        printf("MINISHELL_ERROR : %#08x\n", status);
-}
