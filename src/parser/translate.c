2339a0ccea3da80b4bb8c6d9cb555e8f208645c9
Mohamed Aymane Zary <mzary@e1r5p8.1337.ma>
2025-05-24 16:29:15 +0100

    default message

--- src/parser/translate.c
+++ src/parser/translate.c
@@ -6,14 +6,14 @@
 /*   By: mzary <mzary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 11:33:53 by mzary             #+#    #+#             */
-/*   Updated: 2025/05/23 19:21:34 by mzary            ###   ########.fr       */
+/*   Updated: 2025/05/24 15:45:02 by mzary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/parser.h"
 
 static t_status	update(t_token *token, t_env *env, t_args args);
-static bool		check_flag(t_token *token);
+static bool		check_flag(t_token *token, bool	prev_flag);
 
 t_status	minishell_translate(t_token *root, t_env *env, char *str_exitcode)
 {
@@ -21,7 +21,7 @@ t_status	minishell_translate(t_token *root, t_env *env, char *str_exitcode)
 	t_args		args;
 
 	args.exit = str_exitcode;
-	args.flag = check_flag(root);
+	args.flag = check_flag(root, true);
 	status = update(root, env, args);
 	minishell_free((void **)&str_exitcode);
 	if (status)
@@ -50,17 +50,17 @@ static t_status	update(t_token *token, t_env *env, t_args args)
 				return (s);
 		}
 	}
-	args.flag = check_flag(token);
+	args.flag = check_flag(token, args.flag);
 	s = update(token->right, env, args);
 	if (s)
 		return (s);
 	return (STATUS_SUCCESS);
 }
 
-static bool	check_flag(t_token *token)
+static bool	check_flag(t_token *token, bool prev_flag)
 {
 	if (token->ttype == TTOKEN_COMMAND && token->tvalue
 		&& minishell_strequal(token->tvalue, EXPORT))
 		return (false);
-	return (true);
+	return (prev_flag);
 }
